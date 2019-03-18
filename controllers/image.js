97fc0130e00520f93f3fd71b4f49ce61e270c2a5
Ayush Nair <ayushnair.noida@gmail.com>
2019-03-18 14:54:35 +0530

    Created separate controller files and create new endpoint for Clarifai call

---
+++ controllers/image.js
@@ -0,0 +1,20 @@
+const Clarifai = require('clarifai');
+const app = new Clarifai.App({ apiKey: '61e34b186fda458494bc6aa8f68598af' });
+
+const handleClarifaiCall = (req,res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+}
+
+const handleImagePut = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries',1)
+        .returning('entries')
+        .then(entries => res.json(entries[0]))
+        .catch(err => res.status(400).json("Unable to get entries"));
+}
+
+module.exports = {
+    handleImagePut: handleImagePut,
+    handleClarifaiCall: handleClarifaiCall
+}
--- server.js
+++ server.js
@@ -2,79 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-var knex = require('knex')
+const knex = require('knex')
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+const profile = require('./controllers/profile');
 
 const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',
-      user : 'your_database_user',
-      password : 'your_database_password',
+      user : 'postgres',
+      password : 'test',
       database : 'smartbrain'
     }
-  });
-
+});
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.get('/', (req,res) => {
-    res.json(db.users);
-})
-app.post('/signin', (req,res) => {
-    db.select('email','hash').from('login')
-        .where('email','=',req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid) {
-                return db.select('*').from('users')
-                    .where('email','=',req.body.email)
-                    .then(user => res.json(user[0]))
-                    .catch(err => res.status(400).json("Unable to get user"))
-            } else {
-                res.status(400).json('Wrong credentials')
-            }
-        }).catch(err => res.status(400).json("Wrong credentials"));
-})
-app.post('/register', (req, res) => {
-    const { email, name, password} = req.body;
-    var hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            email: email,
-            hash: hash
-        }).into('login').returning('email')
-            .then(loginEmail =>{
-                return trx('users').returning('*')
-                    .insert({
-                        name: name,
-                        email: loginEmail[0],
-                        joined: new Date()
-                    }).then(user => res.json(user[0]))
-            }).then(trx.commit)
-            .catch(trx.rollback)
-    }).catch(err => res.status(400).json('Unable to register'));
-})
-app.get('/profile/:id', (req, res) =>{
-    const { id } = req.params;
-    db.select('*').from('users').where({id})
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('No such user')
-            }
-        })
-        .catch(err => res.status(400).json('Error getting user'))
-})
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries',1)
-        .returning('entries')
-        .then(entries => res.json(entries[0]))
-        .catch(err => res.status(400).json("Unable to get entries"));
-})
+
+app.get('/', (req,res) => { res.json(db.users) });
+app.post('/signin', (req,res) => { signin.handleSigninPost(req,res,db,bcrypt) });
+app.post('/register', (req, res) => { register.handleRegisterPost(req,res,db,bcrypt) });
+app.get('/profile/:id', (req, res) =>{ profile.handleProfileGet(req,res,db) });
+app.put('/image', (req, res) => { image.handleImagePut(req,res,db) });
+app.post('/imageurl', (req, res) => { image.handleClarifaiCall(req,res) });
 app.listen(3000, (err) => {
     if (err) {
         throw err;
